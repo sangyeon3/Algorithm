fc2d7d7f21cc334f119ab9277e4d280e56c2f0c5
sangyeon3 <68235938+sangyeon3@users.noreply.github.com>
2023-03-11 02:01:19 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 24.74 ms, Memory: 26.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
@@ -0,0 +1,40 @@
+import Foundation
+
+func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
+    
+    let queueSize = queue1.count
+    var sum1 = queue1.reduce(0, +)
+    var sum2 = queue2.reduce(0, +)
+    let halfSum = (sum1 + sum2) / 2
+    
+    var q1 = queue1, q2 = queue2
+    var idx1 = 0, idx2 = 0, count = 0
+    var ans = -1
+    
+    while true {
+        if sum1 == halfSum {
+            return count
+        }
+        if idx1 >= queueSize + idx2 || idx2 >= queueSize + idx1 { 
+            break
+        }
+        if count > queueSize * 2 + 10 { 
+            break
+        }
+
+        if sum1 < halfSum {
+            q1.append(q2[idx2])
+            sum1 += q2[idx2]
+            sum2 -= q2[idx2]
+            idx2 += 1
+        } else {
+            q2.append(q1[idx1])
+            sum1 -= q1[idx1]
+            sum2 += q1[idx1]
+            idx1 += 1
+        }
+        count += 1
+    }
+
+    return -1
+}
