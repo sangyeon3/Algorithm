9f691db7a6cada3f8b06e67a4ea50db6b0b47f41
sangyeon3 <68235938+sangyeon3@users.noreply.github.com>
2023-03-12 18:51:23 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 23.70 ms, Memory: 25.5 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.swift
@@ -3,34 +3,25 @@ import Foundation
 func solution(_ queue1:[Int], _ queue2:[Int]) -> Int {
     
     let queueSize = queue1.count
-    var sum1 = queue1.reduce(0, +)
-    var sum2 = queue2.reduce(0, +)
-    let halfSum = (sum1 + sum2) / 2
+    let queue = queue1 + queue2
+    var idx1 = 0, idx2 = queueSize
+    var sum1 = queue1.reduce(0, +), sum2 = queue2.reduce(0, +)
+    var ans = 0
     
-    var q1 = queue1, q2 = queue2
-    var idx1 = 0, idx2 = 0, count = 0
-    var ans = -1
-    
-    while idx1 < queueSize + idx2 && idx2 < queueSize + idx1 {
-        if sum1 == halfSum {
-            return count
-        }
-        if count > queueSize * 2 + 10 {
-            break
-        }
-        
-        if sum1 < halfSum {
-            q1.append(q2[idx2])
-            sum1 += q2[idx2]
-            sum2 -= q2[idx2]
-            idx2 += 1
+    while true {
+        if sum1 == sum2 { return ans }
+        else if sum1 > sum2 {
+            sum1 -= queue[idx1]
+            sum2 += queue[idx1]
+            idx1 = (idx1 + 1) % (queueSize * 2)
         } else {
-            q2.append(q1[idx1])
-            sum1 -= q1[idx1]
-            sum2 += q1[idx1]
-            idx1 += 1
+            sum1 += queue[idx2]
+            sum2 -= queue[idx2]
+            idx2 = (idx2 + 1) % (queueSize * 2)
         }
-        count += 1
+        ans += 1
+        
+        if ans > 2 * queueSize + 5 { break }
     }
 
     return -1
