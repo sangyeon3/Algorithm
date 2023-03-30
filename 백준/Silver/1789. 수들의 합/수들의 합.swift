9a8b21a91c7279ac329b0b9f47e68ad3e4fc4727
sangyeon3 <68235938+sangyeon3@users.noreply.github.com>
2023-03-31 01:14:38 +0900

    [Silver V] Title: 수들의 합, Time: 12 ms, Memory: 79508 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.swift
+++ 백준/Silver/1789. 수들의 합/수들의 합.swift
@@ -1,12 +1,11 @@
 import Foundation
 
 let S = Int(readLine()!)!
-var (cur, length, num) = (1, 2, 1)
+var (length, num) = (2, 1)
 
 while num + length - 1 < S {
     num += length
-    cur += 1
     length += 1
 }
 
-print(cur)
+print(length-1)
