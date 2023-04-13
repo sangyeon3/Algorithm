f8ee0fe3e832e84fedef19a3b892495a5ce2ced8
sangyeon3 <68235938+sangyeon3@users.noreply.github.com>
2023-04-14 02:34:34 +0900

    [unrated] Title: 요격 시스템, Time: 0.08 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.swift
+++ 프로그래머스/unrated/181188. 요격 시스템/요격 시스템.swift
@@ -5,23 +5,18 @@ func solution(_ targets:[[Int]]) -> Int {
         if $0[0] == $1[0] { return $0[1] < $1[1] }
         return $0[0] < $1[0]
     }
-    var scopes = [[Int]]([targets[0]])
-    var scopeCount = 1
-    
     if targets.count == 1 { return 1 }
     
+    var end = targets[0][1], count = 1
+    
     for i in 1..<targets.count {
-        let scope = scopes[scopeCount-1]
-        if scope[1] <= targets[i][0] {
-            scopes.append(targets[i])
-            scopeCount += 1
+        if end <= targets[i][0] {
+            end = targets[i][1]
+            count += 1
         } else {
-            scopes[scopeCount-1][0] = targets[i][0]
-            if targets[i][1] < scopes[scopeCount-1][1] {
-                scopes[scopeCount-1][1] = targets[i][1]
-            }
+            end = min(end, targets[i][1])
         }
     }
     
-    return scopeCount
+    return count
 }
